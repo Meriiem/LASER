989b9638eec4e8449a8e81c00111b64c764f4632
Meriem <meriemaoudia2@gmail.com>
2023-09-18 03:50:41 +0400

    Modifications

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div className="text-white">
-      <h1 className="w-full text-3xl text-[#00df9a]">REACT</h1>
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+      <h1 id="logo" className="w-full text-3xl text-[#00df9a]">
+        LASER
+      </h1>
 
       <ul className="flex justify-between text-white">
         <li className="p-4">Home</li>
