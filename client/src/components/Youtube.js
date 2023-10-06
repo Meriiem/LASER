e879b42fe4f1b76c1b266ac8cc1774ca3e64db9f
omaralal <109959768+omaralal@users.noreply.github.com>
2023-10-06 17:47:10 +0400

    Create Youtube.js

---
+++ client/src/components/Youtube.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactPlayer from 'react-player/youtube'
+
+export const Youtube = ({youtubeLink}) => {
+
+    return (
+
+        <ReactPlayer style={{paddingLeft:"10%"}} width="100%"height="400px" url={youtubeLink}/>
+        
+    )
+}
