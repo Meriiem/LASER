8fc9a4921173b3b1cf2bd6cb258b60f0077a265b
Meriem <90469517+Meriiem@users.noreply.github.com>
2023-10-28 17:29:05 +0400

    Update Form.js

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,54 +1,61 @@
-import React,{useState} from 'react'
-import axios from "axios"
+import React, { useState } from "react";
+import axios from "axios";
 
+export const Form = ({ setYoutubeLink, setYoutubeVal }) => {
+  const [input, setInput] = useState("");
+  const [youtubeeVal, setYoutubeeVal] = useState("");
 
- 
-export const Form = ({setYoutubeLink, setYoutubeVal}) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setYoutubeLink(input);
 
-    const [input, setInput] = useState('');
-    const [youtubeeVal, setYoutubeeVal] = useState('');
+    // setYoutubeVal(input);
+    axios
+      .post("http://localhost:5000/youtubeUpload", { link: input })
+      .then((res) => {
+        let result = res.data;
+        console.log(result);
+        setYoutubeeVal(result["Youtubetranscript"]);
+        setYoutubeVal(result["Youtubetranscript"]);
+      });
 
-    const handleSubmit= async (e)=>{
-        e.preventDefault();
-        setYoutubeLink(input);
-      
-        // setYoutubeVal(input);
-        axios.post('http://localhost:5000/youtubeUpload',{"link":input}).then((res) => {
-                let result = res.data
-                 console.log(result);
-                 setYoutubeeVal(result["Youtubetranscript"])
-                 setYoutubeVal(result["Youtubetranscript"])
-                
-                }
-                
-            )
-        
-        //empty form
-        setInput('');
-       
-    }
+    //empty form
+    setInput("");
+  };
 
+  return (
+    <form
+      className="form-group custom-form"
+      onSubmit={handleSubmit}
+      method="post"
+    >
+      <label>Enter Youtube URL</label>
 
-   return(
+      <input
+        type="text"
+        id="youtubeVariable"
+        style={{ width: "40%", textAlign: "center", borderRadius: "10px" }}
+        className="form-control custom-input"
+        placeholder="Enter Youtube URL"
+        required
+        onChange={(e) => setInput(e.target.value)}
+        value={input || ""}
+      />
 
-        <form className='form-group custom-form' onSubmit={handleSubmit} method = "post"> 
-
-
-            <label>Enter Youtube URL</label>
-
-            <input  type='text'id="youtubeVariable" style={{width:"40%", textAlign:"center",borderRadius:"10px"}} className='form-control custom-input'
-            placeholder='Enter Youtube URL' required
-            onChange={(e)=>setInput(e.target.value)} value={input||''} />
-
-
-            <button type='submit'style={{backgroundColor:"blue",marginLeft:"20px",borderRadius:"10px",width:"100px",height:"25px" }}className='btn btn-success btn-md'>
-                Generate
-            </button>
-            <br></br>
-
-
-        </form>
-
-
-   )
-   }
+      <button
+        type="submit"
+        style={{
+          backgroundColor: "blue",
+          marginLeft: "20px",
+          borderRadius: "10px",
+          width: "100px",
+          height: "25px",
+        }}
+        className="btn btn-success btn-md"
+      >
+        Generate
+      </button>
+      <br></br>
+    </form>
+  );
+};
