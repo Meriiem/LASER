cacdb02b51ce0d8a6cdf9490d10454c3b1af50f7
Meriem <90469517+Meriiem@users.noreply.github.com>
2023-10-28 17:29:49 +0400

    Update Youtube.js

--- client/src/components/Youtube.js
+++ client/src/components/Youtube.js
@@ -1,14 +1,16 @@
-// this file is to display the video player
-
-import React from 'react'
+import React from "react";
 //note : you need to install "npm install react-player"
-import ReactPlayer from 'react-player/youtube'
+import ReactPlayer from "react-player/youtube";
 
-export const Youtube = ({youtubeLink}) => {
+export const Youtube = ({ youtubeLink }) => {
+  return (
+    <ReactPlayer
+      style={{ paddingLeft: "10%" }}
+      width="100%"
+      height="400px"
+      url={youtubeLink}
+    />
+  );
+};
 
-    return (
-
-        <ReactPlayer style={{paddingLeft:"10%"}} width="100%"height="400px" url={youtubeLink}/>
-        
-    )
-}
+export default Youtube;
