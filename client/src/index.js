9dc59c3e77cdf8c8efd0e2bb3b8fc3a6ce78af46
Meriem <90469517+Meriiem@users.noreply.github.com>
2023-10-28 17:30:58 +0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from "react-dom";
 import "./App.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { Navigation, Home, Account, Upload, Footer, Trial } from "./pages";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+ReactDOM.render(
+  <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/upload" element={<Upload />} />
+      <Route path="/account" element={<Account />} />
+    </Routes>
+    <Footer />
+  </Router>,
+
+  document.getElementById("root")
 );
+
+serviceWorker.unregister();
