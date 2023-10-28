1dd02115503edc0c86ae85a9a92dbf10d63a214a
Meriem <90469517+Meriiem@users.noreply.github.com>
2023-10-28 17:33:47 +0400

    Create Footer.jsx

---
+++ client/src/pages/Footer.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagram,
+  FaTwitterSquare,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-white">
+      <div>
+        <h1 id="logo" className="w-full text-3xl font-medium text-white">
+          LASER
+        </h1>
+        <p className="py-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium
+          lectus quam id leo. Turpis in eu mi bibendum neque.
+        </p>
+        <div className="flex justify-between md:w-[75%] my-6">
+          <FaFacebookSquare size={30} />
+          <FaGithubSquare size={30} />
+          <FaInstagram size={30} />
+          <FaTwitterSquare size={30} />
+        </div>
+
+        <p className="justify-center ">
+          Copyright © 2023 Senior Team. All Rights Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
