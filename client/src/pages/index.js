d201d4061250a25945090084aa380bb8e4b79b91
Meriem <90469517+Meriiem@users.noreply.github.com>
2023-10-28 17:34:13 +0400

    Create index.js

---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Navigation } from "./Navigation";
+export { default as Home } from "./Home";
+export { default as Account } from "./About";
+export { default as Upload } from "./Upload";
+export { default as Footer } from "./Footer";
+export { default as Trial } from "./Trial";
